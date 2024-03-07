a82419fda26e628a54ca78d3844738c65b8e0fdf
kethelynsantos <kethelyngabrielly14@gmail.com>
2024-03-07 14:38:25 -0300

    feat: create a viewset for transporter registration

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import CustomUserViewSet, ClientViewSet
+from .views import CustomUserViewSet, ClientViewSet, TransporterViewSet
 
 
 router = SimpleRouter()
 router.register('customuser', CustomUserViewSet)
 router.register('client', ClientViewSet)
+router.register('transporter', TransporterViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
--- core/views.py
+++ core/views.py
@@ -67,4 +67,52 @@ class ClientViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.data, status=status.HTTP_200_OK)  
     
-    
+
+# gerencia transporters
+class TransporterViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.TransporterSerializer
+    queryset = models.Transporter.objects.all()
+    filter_backends = (filters.DjangoFilterBackend, )
+    filterset_fields = ('user',)
+    search_fields = ('user__username', 'user__email')
+
+    # chamada quando um novo transporter é criado
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # deleta o usuário associado
+        if instance.user:
+            user_id = instance.user.id
+            user = get_object_or_404(models.CustomUser, id=user_id)
+            user.delete()
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # atualiza dados do transporter e do usuário associado
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        # atualizar dados do usuário
+        user_data = request.data.get('user', {})
+        if user_data:
+            user_instance = instance.user
+            user_serializer = serializers.CustomUserSerializer(user_instance, data=user_data, partial=True)
+            user_serializer.is_valid(raise_exception=True)
+            user_serializer.save()
+
+            # desloga o usuário após a atualização da senha
+            Token.objects.filter(user=user_instance).delete()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)  
